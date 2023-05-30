29dbd72170f52a0f20bab0cfca5893a3055fcc82
keagud <keagud@protonmail.com>
2023-05-30 12:41:36 -0700

    add basic resume integration

--- resume/apps.py
+++ resume/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+import subprocess
+from pathlib import Path
+from shutil import copy
 
 
 class ResumeConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    verbose_name = "My Resume"
     name = "resume"
+
+    def ready(self):
+        resume_build_path = Path(__file__).parent.joinpath("resume")
+        build_script_path = resume_build_path.joinpath("build.py").as_posix()
+        resume_pdf_path = resume_build_path.joinpath("resume.pdf").as_posix()
+        build_step = subprocess.run(["python3", build_script_path])
+        build_step.check_returncode()
+
+        copy(resume_pdf_path, Path(__file__).parent)
