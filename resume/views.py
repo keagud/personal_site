cd447e902d47d420fe0b274b8acacc0eec9903a5
keagud <keagud@protonmail.com>
2023-06-12 16:48:22 -0700

    add resume materials

--- resume/views.py
+++ resume/views.py
@@ -4,15 +4,3 @@ from django.http import FileResponse, Http404, HttpResponse
 from django.shortcuts import render
 
 
-def pdf_view(request):
-    pdf_path = Path(__file__).parent.joinpath("resume/resume.pdf")
-
-    try:
-        with open(pdf_path, "rb") as pdf:
-            response = HttpResponse(pdf.read(), content_type="application/pdf")
-            response["Content-Disposition"] = "inline;filename=resume.pdf"
-
-            return response
-
-    except FileNotFoundError:
-        raise Http404()
