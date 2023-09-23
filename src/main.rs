4be0b28841b768107728311324bad9bee0f203c0
keagud <keagud@protonmail.com>
2023-09-22 17:14:26 -0700

    adventures in encoding

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use axum::{routing::get, Router, Server};
+use axum::{
+    headers::authorization::Bearer,
+    routing::{get, post},
+    Json, Router, Server, TypedHeader,
+};
 
 pub mod blog;
 
 pub mod common {
+    use hex;
+    use html5ever::tokenizer::Token::DoctypeToken;
     use serde::{Deserialize, Serialize};
     use sha3::{Digest, Sha3_512};
+    use std::option_env;
     use std::path::PathBuf;
 
     use chrono::{DateTime, NaiveDateTime, Utc};
@@ -43,18 +50,22 @@ pub mod common {
     pub fn validate_token(token: impl AsRef<[u8]>) -> bool {
         const SECRET: &str = dotenv!("SECRET");
 
-        /*
-        let cleaned_bytes = match String::from_utf8(SECRET.into()) {
-            Ok(ref s) => s.trim().as_bytes().to_owned(),
-            _ => return false,
-        };
-        */
+        let secret_bytes =
+            hex::decode(SECRET).expect("Hardcoded secret hex should represent valid bytes");
 
-        let mut hasher = Sha3_512::new();
-        hasher.update(SECRET);
-        let result = hasher.finalize().into_iter().collect::<Vec<u8>>();
+        if let Ok(token_bytes) = hex::decode(&token) {
+            let mut hasher = Sha3_512::new();
+            hasher.update(secret_bytes);
+            let result = hasher.finalize();
 
-        result.as_slice() == token.as_ref()
+            result.as_slice() == token_bytes
+        } else {
+            false
+        }
+    }
+
+    pub fn decode_base64(encoded: impl AsRef<[u8]>) -> anyhow::Result<String> {
+        todo!()
     }
 
     pub fn timestamp_date_format(timestamp: usize, format_str: &str) -> String {
@@ -66,72 +77,92 @@ pub mod common {
         dt.format(format_str).to_string()
     }
 
-
     #[cfg(test)]
-    pub mod test{
+    pub mod test {
 
         use crate::common::*;
         use dotenv_codegen::dotenv;
 
-
         #[test]
-        fn test_validator(){
-
+        fn test_validator() {
             const KEY: &str = dotenv!("KEY");
 
             assert!(validate_token(KEY));
-
-
-
-
         }
-
-
     }
 }
 
 pub mod route {
-    use crate::common;
+    use crate::common::{self, Post};
     use anyhow;
+    use anyhow::format_err;
     use axum::{
-        extract,
-        http::StatusCode,
+        body::{Body, HttpBody},
+        extract::{self, Json, Multipart},
+        headers::authorization::Bearer,
+        http::{Request, StatusCode},
         response::{Html, IntoResponse},
+        TypedHeader,
     };
 
-    use crate::blog::{db, render};
-    pub struct RoutingError(anyhow::Error);
+    use serde::{Deserialize, Serialize};
 
-    impl<E> From<E> for RoutingError
+    use crate::blog::{db, render};
+    pub struct SiteError(anyhow::Error, Option<StatusCode>);
+
+    #[derive(Serialize, Deserialize, Default)]
+    pub struct PostUpload {
+        pub title: String,
+        pub timestamp: usize,
+        pub slug: String,
+        pub file_content_base64: String,
+    }
+
+    impl PostUpload {
+        pub fn save(&self) -> anyhow::Result<Post> {
+            // decode file content from base 64, save it
+            // to the correct path to a file named slug.md.
+            // Return a Post on success.
+            todo!()
+        }
+    }
+
+    impl SiteError {
+        pub fn from_status(s: StatusCode) -> Self {
+            Self(format_err!("{:?}", s), Some(s))
+        }
+    }
+
+    impl<E> From<E> for SiteError
     where
         E: Into<anyhow::Error>,
     {
         fn from(err: E) -> Self {
-            Self(err.into())
+            Self(err.into(), None)
         }
     }
-    impl IntoResponse for RoutingError {
+    impl IntoResponse for SiteError {
         fn into_response(self) -> axum::response::Response {
             if cfg!(debug_assertions) {
                 tracing::debug!("{}", self.0.backtrace());
             }
 
             (
-                StatusCode::INTERNAL_SERVER_ERROR,
+                self.1.unwrap_or(StatusCode::INTERNAL_SERVER_ERROR),
                 format!("Something went wrong: {:?}", self.0),
             )
                 .into_response()
         }
     }
 
-    pub async fn posts_list() -> Result<Html<String>, RoutingError> {
+    pub async fn posts_list() -> Result<Html<String>, SiteError> {
         let posts = db::DbConnection::new()?.all_posts()?;
         let posts_list = render::post_index_display(&posts)?;
         let content = render::render_html_str("Posts Index", &posts_list)?;
         Ok(Html::from(content))
     }
 
-    pub async fn home() -> Result<Html<String>, RoutingError> {
+    pub async fn home() -> Result<Html<String>, SiteError> {
         let content = render::read_file_contents(common::HOMEPAGE_PATH)
             .and_then(|ref s| render::render_html_str("Home", s))
             .map(Html::from)?;
@@ -139,13 +170,25 @@ pub mod route {
         Ok(content)
     }
 
-    pub async fn post(
+    pub async fn get_post(
         extract::Path(slug): extract::Path<String>,
-    ) -> Result<Html<String>, RoutingError> {
+    ) -> Result<Html<String>, SiteError> {
         let post = db::DbConnection::new()?.get(&slug)?;
         let content = render::render_md(&post.title, post.md_path())?;
 
         Ok(Html::from(content))
+    }
+
+    pub async fn add_new_post(
+        TypedHeader(bearer_auth): TypedHeader<Bearer>,
+        Json(payload): Json<PostUpload>,
+    ) -> Result<StatusCode, SiteError> {
+        if !common::validate_token(bearer_auth.token()) {
+            Err(SiteError::from_status(StatusCode::FORBIDDEN))
+        } else {
+            // TODO save to database here
+            payload.save().map(|_| StatusCode::OK).map_err(|e| e.into())
+        }
     }
 }
 
@@ -161,8 +204,8 @@ async fn main() -> anyhow::Result<()> {
         .route("/", get(route::home))
         .route("/blog", get(route::posts_list))
         .route("/blog/", get(route::posts_list))
-        .route("/blog/:slug", get(route::post))
-        .route("/blog/:slug/", get(route::post));
+        .route("/blog/:slug", get(route::get_post))
+        .route("/blog/:slug/", get(route::get_post));
 
     let port = "8000";
     let host = "0.0.0.0";
