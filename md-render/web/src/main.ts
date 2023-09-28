0def8cf69393c1188275d6f74fed1c77b4ada607
keagud <keagud@protonmail.com>
2023-09-28 12:38:16 -0700

    more svelte tweaks

--- md-render/web/src/main.ts
+++ md-render/web/src/main.ts
@@ -1,14 +1,6 @@
-import './app.css'
+import './pico.min.css'
 import App from './App.svelte'
 
-import * as foo from "lib-rs";
-
-
-const init = async () => {
-
-  await import("lib-rs");
-
-}
 
 const app = new App({
   target: document.getElementById('app')!,
